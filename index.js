a9118ea9dfd92e616111f0a4f4936524091fd5f5
Nikita <nikitakubavat33@gmail.com>
2022-06-12 11:05:16 +0530

    don

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const connectToMongo = require('./db')
 const express = require('express')
-const cors = require('cors');
+const cors = require('cors')
+const path = require('path')
 const app = express()
 
 connectToMongo()
