5f5d7e9abd189c3f83a598550cb7a1f94335451d
Nikita <nikitakubavat33@gmail.com>
2022-08-12 16:29:20 +0530

    navbar fixed

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,29 +1,56 @@
-import React from 'react'
-import { Link, useLocation , useNavigate} from "react-router-dom";
-
-  
+import React from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-    let location = useLocation();
-    let navigate = useNavigate()
-    const handlLogout = () => {
-        localStorage.removeItem('auth-token');
-        navigate("/login")
-    }
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container-fluid ">
-                <Link className="navbar-brand" to="/">iNotes</Link>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    {!localStorage.getItem('auth-token')?<form className="d-flex"> 
-                    <Link className="btn btn-primary mx-1" to="/login" role="button">Login</Link>
-                    <Link className="btn btn-primary mx-1" to="/signup" role="button">Signup</Link>
-                    </form> :
-                    <button className='btn btn-primary' onClick={handlLogout}>Logout</button>}
-                </div>
-            </div>
-        </nav>
-    )
-}
+  let location = useLocation();
+  let navigate = useNavigate();
+  const handlLogout = () => {
+    localStorage.removeItem("auth-token");
+    navigate("/login");
+  };
+  return (
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <Link to="/" class="navbar-brand">
+          iNotes
+        </Link>
+        <button
+          type="button"
+          class="navbar-toggler"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarCollapse"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <div class="navbar-nav ms-auto">
+            {!localStorage.getItem("auth-token") ? (
+              <form>
+                <Link
+                  className="btn btn-primary mx-1"
+                  to="/login"
+                  role="button"
+                >
+                  Login
+                </Link>
+                <Link
+                  className="btn btn-primary mx-1"
+                  to="/signup"
+                  role="button"
+                >
+                  Signup
+                </Link>
+              </form>
+            ) : (
+              <button className="btn btn-primary" onClick={handlLogout}>
+                Logout
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
